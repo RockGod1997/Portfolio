5b16a0855b1d44652389f8ce41fe62cf4bc1fbe9
Samarth Agrawal <agrawal.samarth123@gmail.com>
2025-05-01 20:57:43 +0100

    updated slider style

--- my-portfolio/app/components/slider.tsx
+++ my-portfolio/app/components/slider.tsx
@@ -8,7 +8,7 @@ const Subtitle: string = "";
 const Date: string = "";
 
 const Card = ({ title = Title, subtitle = Subtitle, date = Date }) => (
-  <div className="bg-gray-800 bg-opacity-30 p-6 rounded-lg border border-gray-600 hover:shadow-2xl transition-colors duration-300 shadow-md text-center flex flex-col items-center justify-center">
+  <div className="bg-gray-800 bg-opacity-30 p-6 rounded-xl border border-gray-600 hover:shadow-2xl transition-colors duration-300 shadow-md text-center flex flex-col items-center justify-center min-h-[240px] w-[360px]">
     <h3 className="text-gray-100 text-lg">{title}</h3>
     {subtitle && <p className="text-gray-300 mt-2 text-sm">{subtitle}</p>}
     {date && <p className="text-gray-300 mt-2 text-sm">{date}</p>}
@@ -19,7 +19,8 @@ const Slider = () => {
   const educationData = [
     {
       title: "Trinity College Dublin",
-      subtitle: "MSc in Computer Science, Future Networked System",
+      subtitle: `MSc in Computer Science, 
+        Future Networked System`,
       date: "2023 - 2024",
     },
     {
@@ -62,7 +63,7 @@ const Slider = () => {
             <h2 className="text-xl sm:text-2xl text-gray-200 text-center font-semibold mb-4">
               Education
             </h2>
-            <div className="grid grid-cols-1 gap-6">
+            <div className="gap-6 flex items-center justify-center">
               {educationData.map((edu, index) => (
                 <Card key={index} {...edu} />
               ))}
@@ -76,9 +77,9 @@ const Slider = () => {
             <h2 className="text-xl sm:text-2xl text-gray-200 text-center font-semibold mb-4">
               Work Experience
             </h2>
-            <div className="grid grid-cols-1 gap-6">
+            <div className="gap-6 flex items-center justify-center">
               {workExperienceData.map((work, index) => (
-                <Card key={index} {...work} />
+              <Card key={index} {...work} />
               ))}
             </div>
           </section>
