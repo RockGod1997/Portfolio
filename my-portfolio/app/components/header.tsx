84132c3ae91eed7e9855c199e8bc386ec3315ffb
Samarth Agrawal <agrawal.samarth123@gmail.com>
2025-05-24 12:42:30 +0100

    updated image pos

--- my-portfolio/app/components/header.tsx
+++ my-portfolio/app/components/header.tsx
@@ -1,22 +1,24 @@
 import TypingEffect from "./typingeffect";
-import Image from "next/image";
 export default function Header() {
   return (
     <header className="flex flex-col items-center gap-2 row-start-1">
       <div className="flex flex-col items-center gap-2">
         {" "}
-        <Image
-          src="/Profilepic.jpg"
-          alt="profile"
-          className="rounded-full object-cover w-24 h-24 border-2 border-gray-300"
-          style={{ objectPosition: "center" }}
-          width={150}
-          height={150}
-        />
-        <h1 className="text-4xl text-gray-100 font-bold text-center">Samarth Agrawal</h1>
+        <div
+          className="w-24 h-24 rounded-full overflow-hidden border-2 border-gray-300"
+          style={{
+            backgroundImage: "url('/Profilepic.jpg')",
+            backgroundPosition: "37% center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "170%",
+          }}
+        ></div>
+        <h1 className="text-4xl text-gray-100 font-bold text-center">
+          Samarth Agrawal
+        </h1>
       </div>
       <p className="text-lg text-gray-100 text-center">
-        <TypingEffect text="Software Engineer"/>
+        <TypingEffect text="Software Engineer" />
       </p>
     </header>
   );
